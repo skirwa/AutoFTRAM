3de4dfe886a042de1baa03cf146c50b846aa3b4f
Sharon <skirwa44@gmail.com>
2022-11-14 17:20:34 -0500

    Documented the code

--- Algorithm-1.R
+++ Algorithm-1.R
@@ -1,8 +1,18 @@
 
+# DB - Database
+# k - Number of  rules that the user wants to be generated.
+# L - A set containing valid rules. If the support of a rule meets the minimum support value,
+#     then the rule is added to the list L.
+#     L contains the k most frequent association rules that meet the minimum support and confidence values.
+# I - A set of items (i, j). 
+
+# User-defined functions to be called:
+# Initialize_Remove, Save, Expand_L, Expand_R
+
 ftarm <- function(DB, k, minconf) {
   R <- list()
   L <- list()
-  # Scan transaction list from db to get the itemSet I 
+  # Scan transaction list from the database to get the itemSet I 
   I <- list()
   
   Initialize_Remove(DB, k, mconf)
@@ -14,10 +24,10 @@ ftarm <- function(DB, k, minconf) {
        newItems <- i.append(j)
        if (sup(newItems) >= minsup) {
          if (conf(i, j) >= minconf) {
-           save(newSet, L, k, minsup)
+           Save(newSet, L, k, minsup)
          }
          if (conf(j, i) >= minconf) {
-           save(newSet, L, k, minsup)
+           Save(newSet, L, k, minsup)
          }
          # compute MAXITEM here
          # update R
@@ -33,10 +43,10 @@ ftarm <- function(DB, k, minconf) {
         } 
       }
       if (maxSupR.expandLR == TRUE) {
-        expand_L(r, L, R, k, minsup, minconf)
-        expand_R(r, L, R, k, minsup, minconf)
+        Expand_L(r, L, R, k, minsup, minconf)
+        Expand_R(r, L, R, k, minsup, minconf)
       } else {
-        expand_R(r, L, R, k, minsup, minconf)
+        Expand_R(r, L, R, k, minsup, minconf)
       }
       
       newR <- list()
